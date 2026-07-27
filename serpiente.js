3267ac8870c59fac2c08938dfab59b32d216c8ae
MelanyNono <melanynono123@gmail.com>
2026-07-27 17:15:55 -0500

    implementar función pintarParte para dibujar una celda

--- serpiente.js
+++ serpiente.js
@@ -22,12 +22,23 @@
     function dibujarTablero(){
       ctx.strokeStyle = "white";
 
-      for (let x = 0; x <= canvas.width; x += TAMANIO_CELDA) {
+    for (let x = 0; x <= canvas.width; x += TAMANIO_CELDA) {
         ctx.beginPath();
         ctx.moveTo(x, 0);
         ctx.lineTo(x, canvas.height);
         ctx.stroke();
-      }
+    }
+
+    function pintarParte(lineaX, lineaY) {
+    let x = lineaX * TAMANIO_CELDA;
+    let y = lineaY * TAMANIO_CELDA;
+
+    ctx.fillStyle = "#22c55e";
+    ctx.fillRect(x, y, TAMANIO_CELDA, TAMANIO_CELDA);
+
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(x, y, TAMANIO_CELDA, TAMANIO_CELDA);
+}
 
       for (let y = 0; y <= canvas.height; y += TAMANIO_CELDA) {
         ctx.beginPath();
